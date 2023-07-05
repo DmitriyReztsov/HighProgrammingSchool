63c8ed440c61dfd2b4850324c27eaf47babbf2c9
DmitriyReztsov <rezcov_d@mail.ru>
2023-07-05 11:23:00 +0300

    added DoubleLinked List

---
+++ lessons/ads/double_linked_list.py
@@ -0,0 +1,101 @@
+from typing import Any, List, Optional
+
+
+class Node:
+    def __init__(self, v: Any) -> None:
+        self.value = v
+        self.prev = None
+        self.next = None
+
+
+class LinkedList2:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def add_in_tail(self, item: Node) -> None:
+        if self.head is None:
+            self.head = item
+            item.prev = None
+            item.next = None
+        else:
+            self.tail.next = item
+            item.prev = self.tail
+        self.tail = item
+        self.length += 1
+
+    def find(self, val: Any, from_node: Node = None) -> Optional[Node]:
+        node = self.head if from_node is None else from_node
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val: Any) -> List[Node]:
+        return_list = []
+        node = self.head
+        while node is not None:
+            found_node = self.find(val, node)
+            if found_node is None:
+                break
+            return_list.append(found_node)
+            node = found_node.next
+        return return_list
+
+    def _delete(self, node: Node) -> None:
+        if self.head == node and self.tail == node:
+            self.head, self.tail = None, None
+        elif self.head == node:
+            self.head = node.next
+            node.next.prev, node.next = None, None
+        elif self.tail == node:
+            self.tail = node.prev
+            node.prev.next, node.prev = None, None
+        else:
+            node.next.prev, node.prev.next = node.prev, node.next
+            node.prev, node.next = None, None
+        self.length -= 1 if self.length > 0 else 0
+
+    def delete(self, val: Any, all: bool = False) -> None:
+        if all:
+            nodes = self.find_all(val)
+        else:
+            nodes = [self.find(val)]
+        for node in nodes:
+            self._delete(node)
+
+    def clean(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def len(self):
+        return self.length
+
+    def insert(self, after_node: Node, new_node: Node) -> None:
+        if after_node is None and self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        elif after_node is None:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            new_node.prev, new_node.next = after_node, after_node.next
+            if after_node.next is not None:
+                after_node.next.prev = new_node
+            after_node.next = new_node
+            if self.tail is after_node:
+                self.tail = new_node
+        self.length += 1
+
+    def add_in_head(self, new_node):
+        new_node.next = self.head
+        if self.head is not None:
+            self.head.prev = new_node
+        if self.tail is None:
+            self.tail = new_node
+        self.head = new_node
+        self.length += 1
--- tests/ads/conftest.py
+++ tests/ads/conftest.py
@@ -1,5 +1,7 @@
 from pytest import fixture
 
+from lessons.ads.double_linked_list import LinkedList2
+from lessons.ads.double_linked_list import Node as Node2
 from lessons.ads.linked_list import LinkedList, Node
 
 
@@ -536,4 +538,391 @@ def setup_instances_to_sum_negative():
     s_list_2.head = n1
     s_list_2.tail = n7
     instances_list.append((s_list_1, s_list_2))
+    return instances_list
+
+
+@fixture
+def setup_instances_to_find():
+    instances_list = []
+    s_list = LinkedList2()
+    instances_list.append((s_list, 12, None))
+
+    n1 = Node2(12)
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n1
+    instances_list.append((s_list, 12, n1))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n1.next = n2
+    n2.prev = n1
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n2
+    instances_list.append((s_list, 55, n2))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n3 = Node2(100)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n3
+    instances_list.append((s_list, 55, n2))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n3 = Node2(100)
+    n4 = Node2(99)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    n3.next, n4.prev = n4, n3
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n4
+    instances_list.append((s_list, 5500, None))
+
+    return instances_list
+
+
+@fixture
+def setup_instances_to_find_all_2():
+    instances_list = []
+    s_list = LinkedList2()
+    instances_list.append((s_list, 12, []))
+
+    n1 = Node2(12)
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n1
+    instances_list.append((s_list, 12, [n1]))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n1.next = n2
+    n2.prev = n1
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n2
+    instances_list.append((s_list, 55, [n2]))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n3 = Node2(100)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n3
+    instances_list.append((s_list, 55, [n2]))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n3 = Node2(100)
+    n4 = Node2(55)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    n3.next, n4.prev = n4, n3
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n4
+    instances_list.append((s_list, 55, [n2, n4]))
+
+    return instances_list
+
+
+@fixture
+def setup_instances_to_delete_2():
+    instances_list = []
+    s_list = LinkedList2()
+    instances_list.append((s_list, 12, None, None))
+
+    n1 = Node2(12)
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n1
+    instances_list.append((s_list, 12, None, None))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n1.next, n2.prev = n2, n1
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n2
+    instances_list.append((s_list, 55, n1, n1))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n3 = Node2(2100)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n3
+    instances_list.append((s_list, 55, n1, n3))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n3 = Node2(2100)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n3
+    instances_list.append((s_list, 12, n2, n3))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n3 = Node2(2100)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n3
+    instances_list.append((s_list, 2100, n1, n2))
+
+    return instances_list
+
+
+@fixture
+def setup_instances_to_delete_all_2():
+    instances_list = []
+    s_list = LinkedList2()
+    instances_list.append((s_list, 12, None, None))
+
+    n1 = Node2(12)
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n1
+    instances_list.append((s_list, 12, None, None))
+
+    n1 = Node2(55)
+    n2 = Node2(55)
+    n1.next, n2.prev = n2, n1
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n2
+    instances_list.append((s_list, 55, None, None))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n3 = Node2(55)
+    n4 = Node2(200)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    n3.next, n4.prev = n4, n3
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n4
+    instances_list.append((s_list, 55, n1, n4))
+
+    n1 = Node2(12)
+    n2 = Node2(55)
+    n3 = Node2(200)
+    n4 = Node2(55)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    n3.next, n4.prev = n4, n3
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n4
+    instances_list.append((s_list, 55, n1, n3))
+
+    n1 = Node2(55)
+    n2 = Node2(12)
+    n3 = Node2(55)
+    n4 = Node2(200)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    n3.next, n4.prev = n4, n3
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n4
+    instances_list.append((s_list, 55, n2, n4))
+
+    return instances_list
+
+
+@fixture
+def setup_instances_to_insert_2():
+    instances_list = []
+    s_list = LinkedList2()
+    n1 = Node2(0)
+    instances_list.append((s_list, None, n1, n1, n1))
+
+    n1 = Node2(0)
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n1
+    n2 = Node2(1)
+    instances_list.append((s_list, None, n2, n1, n2))
+
+    n1 = Node2(0)
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n1
+    n2 = Node2(1)
+    instances_list.append((s_list, n1, n2, n1, n2))
+
+    n1 = Node2(0)
+    n2 = Node2(1)
+    n1.next, n2.prev = n2, n1
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n2
+    n3 = Node2(2)
+    instances_list.append((s_list, None, n3, n1, n3))
+
+    n1 = Node2(0)
+    n2 = Node2(2)
+    n1.next, n2.prev = n2, n1
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n2
+    n3 = Node2(1)
+    instances_list.append((s_list, n1, n3, n1, n2))
+
+    n1 = Node2(0)
+    n2 = Node2(1)
+    n1.next, n2.prev = n2, n1
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n2
+    n3 = Node2(2)
+    instances_list.append((s_list, n2, n3, n1, n3))
+
+    n1 = Node2(0)
+    n2 = Node2(2)
+    n3 = Node2(3)
+    n4 = Node2(4)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    n3.next, n4.prev = n4, n3
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n4
+    n5 = Node2(1)
+    instances_list.append((s_list, n1, n5, n1, n4))
+
+    n1 = Node2(0)
+    n2 = Node2(1)
+    n3 = Node2(3)
+    n4 = Node2(4)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    n3.next, n4.prev = n4, n3
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n4
+    n5 = Node2(2)
+    instances_list.append((s_list, n2, n5, n1, n4))
+
+    n1 = Node2(0)
+    n2 = Node2(1)
+    n3 = Node2(2)
+    n4 = Node2(3)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    n3.next, n4.prev = n4, n3
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n4
+    n5 = Node2(4)
+    instances_list.append((s_list, n4, n5, n1, n5))
+
+    n1 = Node2(0)
+    n2 = Node2(1)
+    n3 = Node2(2)
+    n4 = Node2(3)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    n3.next, n4.prev = n4, n3
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n4
+    n5 = Node2(4)
+    instances_list.append((s_list, None, n5, n1, n5))
+
+    return instances_list
+
+
+@fixture
+def setup_instances_to_add_in_head():
+    instances_list = []
+    s_list = LinkedList2()
+    n1 = Node2(0)
+    instances_list.append((s_list, n1, n1, n1))
+
+    n1 = Node2(0)
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n1
+    n2 = Node2(1)
+    instances_list.append((s_list, n2, n2, n1))
+
+    n1 = Node2(0)
+    n2 = Node2(1)
+    n1.next, n2.prev = n2, n1
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n2
+    n3 = Node2(2)
+    instances_list.append((s_list, n3, n3, n2))
+
+    return instances_list
+
+
+@fixture
+def setup_instances_to_clean_2():
+    instances_list = []
+    s_list = LinkedList2()
+    instances_list.append((s_list, None, None, 0))
+
+    n1 = Node2(0)
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n1
+    instances_list.append((s_list, None, None, 0))
+
+    n1 = Node2(0)
+    n2 = Node2(1)
+    n3 = Node2(2)
+    n4 = Node2(3)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    n3.next, n4.prev = n4, n3
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n4
+    instances_list.append((s_list, None, None, 0))
+
+    return instances_list
+
+
+@fixture
+def setup_instances_to_len_2():
+    instances_list = []
+    s_list = LinkedList2()
+    instances_list.append((s_list, 0))
+
+    n1 = Node2(0)
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n1
+    instances_list.append((s_list, 1))
+
+    n1 = Node2(0)
+    n2 = Node2(1)
+    n3 = Node2(2)
+    n4 = Node2(3)
+    n1.next, n2.prev = n2, n1
+    n2.next, n3.prev = n3, n2
+    n3.next, n4.prev = n4, n3
+    s_list = LinkedList2()
+    s_list.head = n1
+    s_list.tail = n4
+    instances_list.append((s_list, 4))
+
     return instances_list
