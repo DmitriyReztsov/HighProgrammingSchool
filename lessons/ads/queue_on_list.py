8030ccddf26556bba309e0c54b11b231233eac69
DmitriyReztsov <rezcov_d@mail.ru>
2023-07-09 15:24:10 +0300

    queue on list fix

--- lessons/ads/queue_on_list.py
+++ lessons/ads/queue_on_list.py
@@ -10,11 +10,13 @@ class Queue:
     def enqueue(self, item: Any) -> None:
         # вставка в хвост
         self.queue.append(item)
+        self.counter += 1
 
     def dequeue(self) -> Any:
         # выдача из головы
         if self.counter == 0:
             return None # если очередь пустая
+        self.counter -= 1
         return self.queue.pop(0)
 
     def size(self):
