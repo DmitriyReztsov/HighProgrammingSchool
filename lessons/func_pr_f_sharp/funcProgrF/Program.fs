2fbc2bd6b62e6270d22b0a8490793d7f864c46e1
DmitriyReztsov <rezcov_d@mail.ru>
2025-07-18 10:39:38 +0300

    simplify F# first code

--- lessons/func_pr_f_sharp/funcProgrF/Program.fs
+++ lessons/func_pr_f_sharp/funcProgrF/Program.fs
@@ -3,10 +3,4 @@ printfn "Hello, World!!!"
 
 
 // 4.3.2
-let AA: float = 2.1
-let BB: float = 3.2
-let CC: int = 8
-let DD: int = 4
-let EE: float = 1.3
-let result2: float = AA + BB * (float CC / float DD + EE)
-printfn "%f" result2
+printfn "%f" (2.1 + 3.2 * (float 8 / float 4 + 1.3))
