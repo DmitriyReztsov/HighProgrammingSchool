3a3dcabd142210e679feee7a9cefef9fa444ccec
DmitriyReztsov <rezcov_d@mail.ru>
2025-07-18 15:31:52 +0300

    functions

---
+++ lessons/func_pr_f_sharp/funcProgrF/Function.fs
@@ -0,0 +1,9 @@
+let g (n: int) = n + 5 // int -> int
+
+let gg = fun (n: int) -> n + 5
+
+let h (x: float, y: float) = System.Math.Sqrt(x ** 2 + y ** 2)
+
+// printf "%d\n" (g 3)
+// printf "%d\n" (gg 3)
+// printf "%f\n" (h (3, 4))
