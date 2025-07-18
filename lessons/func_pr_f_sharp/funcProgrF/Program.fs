9e94f8ee92e81e635a5fbd8b6154308584698926
DmitriyReztsov <rezcov_d@mail.ru>
2025-07-18 09:41:00 +0300

    F# first code

--- lessons/func_pr_f_sharp/funcProgrF/Program.fs
+++ lessons/func_pr_f_sharp/funcProgrF/Program.fs
@@ -1,18 +1,6 @@
 ﻿// 4.3.1
 printfn "Hello, World!!!"
 
-// 4.3.2
-let X = 2.1 + 3.2 * (8.0 / 4.0 + 1.3)
-printfn "%f" X
-
-// 4.3.2
-let A: float = 2.1
-let B: float = 3.2
-let C: float = 8
-let D: float = 4
-let E: float = 1.3
-let result: float = A + B * (C / D + E)
-printfn "%f" result
 
 // 4.3.2
 let AA: float = 2.1
