742825f81710d1c0255d5802922228936ecb616f
DmitriyReztsov <rezcov_d@mail.ru>
2025-07-18 21:54:16 +0300

    function 3

--- lessons/func_pr_f_sharp/funcProgrF/Function.fs
+++ lessons/func_pr_f_sharp/funcProgrF/Function.fs
@@ -1,6 +1,6 @@
-let g (n: int) = n + 5 // int -> int
+let g (n) = n + 5 // int -> int
 
-let gg = fun (n: int) -> n + 5
+let gg = fun n -> n + 5
 
 let h (x, y) =
     System.Math.Sqrt(float x ** 2 + float y ** 2)
