c5f3d766f6dd7d7908534aef1d8586734e9e6ff3
DmitriyReztsov <rezcov_d@mail.ru>
2025-07-19 23:31:08 +0300

    functions last chance

--- lessons/func_pr_f_sharp/funcProgrF/Function.fs
+++ lessons/func_pr_f_sharp/funcProgrF/Function.fs
@@ -2,5 +2,4 @@ let g (n) = n + 5 // int -> int
 
 let gg = fun n -> n + 5
 
-let h (x, y) =
-    System.Math.Sqrt(float x ** 2 + float y ** 2)
+let h (x, y) = System.Math.Sqrt(float x ** 2 + float y ** 2)
