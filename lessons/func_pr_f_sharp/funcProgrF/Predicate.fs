3d0af3b31b66aed38a3ac64bb5c078b3b7a83fca
DmitriyReztsov <rezcov_d@mail.ru>
2025-07-22 10:07:06 +0300

    Predicate 2

--- lessons/func_pr_f_sharp/funcProgrF/Predicate.fs
+++ lessons/func_pr_f_sharp/funcProgrF/Predicate.fs
@@ -1,16 +1,9 @@
 // 16.1
-let notDivisible (n, m) =
-    if n = 0 then false
-    else m % n = 0
+let notDivisible (n, m) = n <> 0 && m % n = 0
 
 // 16.2
 let prime n =
-    if n <= 1 then false
-    elif n = 2 then true
-    else
-    let mutable pivot = int(System.Math.Sqrt(n))
-    let mutable is_prime = true
-    while pivot > 1 && is_prime do
-        if n % pivot = 0 then is_prime <- false
-        pivot <- pivot - 1
-    is_prime
+    let rec checkDivisor pivot =
+        pivot > 1 && (n % pivot = 0 || checkDivisor (pivot - 1))
+    
+    n > 1 && not (checkDivisor (int(System.Math.Sqrt(n))))
