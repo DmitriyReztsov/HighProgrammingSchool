dbd6db5c08ad8b2e77f02e62638eae38f924e70f
DmitriyReztsov <rezcov_d@mail.ru>
2025-08-03 22:04:16 +0300

    List

---
+++ lessons/func_pr_f_sharp/funcProgrF/src/List.fs
@@ -0,0 +1,26 @@
+namespace src
+
+
+module List =
+    // 34.1
+    let rec upto n =
+        
+        let rec make_list i =
+            match i with
+                | i when i = n -> [i]
+                | i -> i :: make_list (i + 1)
+        make_list 1
+
+    // 34.2
+    let rec dnto n =
+        match n with
+        | n when n = 0 -> []
+        | n -> n :: dnto (n - 1)
+
+    // 34.3
+    let rec evenn n =
+        let rec make_list i =
+            match i with
+                | i when i = n -> [i * 2]
+                | i -> 2 * i :: make_list (i + 1)
+        make_list 1
---
+++ lessons/func_pr_f_sharp/funcProgrF/tests/TestsList.fs
@@ -0,0 +1,30 @@
+open Expecto
+open src.List
+
+[<Tests>]
+let ListTests =
+  testList "List tests" [
+    testCase "List upto 1, [1]" <| fun _ ->
+      Expect.equal (upto 1) [1] "upto 1 = [1]"
+
+    testCase "List upto 5, [1; 2; 3; 4; 5]" <| fun _ ->
+      Expect.equal (upto 5) [1; 2; 3; 4; 5] "upto 5 = [1; 2; 3; 4; 5]"
+
+    testCase "List dnto 1, [1]" <| fun _ ->
+      Expect.equal (dnto 1) [1] "dnto 1 = [1]"
+
+    testCase "List dnto 5, [5; 4; 3; 2; 1]" <| fun _ ->
+      Expect.equal (dnto 5) [5; 4; 3; 2; 1] "dnto 5 = [5; 4; 3; 2; 1]"
+
+    testCase "List evenn 1, [2]" <| fun _ ->
+      Expect.equal (evenn 1) [2] "evenn 1 = [2]"
+
+    testCase "List evenn 5, [2]" <| fun _ ->
+      Expect.equal (evenn 5) [2; 4; 6; 8; 10] "evenn 5 = [2; 4; 6; 8; 10]"
+
+  ]
+
+[<EntryPoint>]
+let main args =
+    runTestsWithCLIArgs [] args ListTests
+
